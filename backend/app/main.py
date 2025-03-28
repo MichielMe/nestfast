366d85f42ee5942bb35f31a5537e4d419ed0d623
Michiel Meire <Michiel.MEIRE@VRT.BE>
2025-03-28 10:52:50 +0100

    auth and crud

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,27 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+import sentry_sdk
+from app.api.v1 import auth_router, workouts_router
+from app.db import Base, engine
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(lifespan=lifespan)
+
+Base.metadata.create_all(bind=engine)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+)
 
 
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+
+
+app.include_router(auth_router)
+app.include_router(workouts_router)
---
+++ backend/app/models/__init__.py
@@ -0,0 +1,3 @@
+from .models import Routine, User, Workout
+
+__all__ = ["User", "Workout", "Routine"]
